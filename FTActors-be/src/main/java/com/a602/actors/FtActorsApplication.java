ab8a9990f41842c65b385141a757411bcd6e6dd4
BaeJinHyeon <compuet33@gmail.com>
2024-03-06 14:04:54 +0900

    REFACTOR : application.yml  파일 환경 변수 설정

--- FTActors-be/src/main/java/com/a602/actors/FtActorsApplication.java
+++ FTActors-be/src/main/java/com/a602/actors/FtActorsApplication.java
@@ -2,8 +2,10 @@ package com.a602.actors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/config.yml")
 public class FtActorsApplication {
 
 	public static void main(String[] args) {
