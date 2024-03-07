f352025c7d74109aa5a428488efd8cb5e574a148
박소영 <soyoung9511@naver.com>
2024-03-07 16:49:39 +0900

    feat: queryDsl 설정

--- FTActors-be/src/main/java/com/a602/actors/FtActorsApplication.java
+++ FTActors-be/src/main/java/com/a602/actors/FtActorsApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @PropertySource("classpath:/config.yml")
-
+@EnableJpaAuditing //리스너 넣을 때 사용
 public class FtActorsApplication {
 
 	public static void main(String[] args) {
