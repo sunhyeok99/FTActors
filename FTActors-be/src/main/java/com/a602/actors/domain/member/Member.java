b8d211a5c27b91dc3103e0cb3a0f5437d40447a1
DESKTOP-LL832F4\SSAFY <wjddn0308@naver.com>
2024-03-08 17:30:07 +0900

    feat : 멤버 회원가입, 로그인, 로그아웃 기능 추가

--- FTActors-be/src/main/java/com/a602/actors/domain/member/Member.java
+++ FTActors-be/src/main/java/com/a602/actors/domain/member/Member.java
@@ -1,4 +1,35 @@
 package com.a602.actors.domain.member;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@DynamicUpdate
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id")
+    private String memberId;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    @Builder
+    public Member(String memberId, String password, String email, String phone){
+        this.memberId = memberId;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+
+    }
 }
--- FTActors-be/src/main/java/com/a602/actors/domain/member/service/Member.java
+++
@@ -1,4 +0,0 @@
-package com.a602.actors.domain.member.service;
-
-public class Member {
-}
