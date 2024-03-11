6f9da642bd0fc0a8c0523bda5c690961471940ad
김도희 <catisaw99@gmail.com>
2024-03-11 16:20:38 +0900

    feat : 상세 페이지 구체화

--- FTActors-fe/src/views/BoardDetail.vue
+++ FTActors-fe/src/views/BoardDetail.vue
@@ -1,15 +1,20 @@
 <template>
-    <div class="about">
-      <h1>상세 공고 페이지</h1>
+  <h1>상세 공고 페이지</h1>
+    <div class="boardpage">
+      <img src="@/assets/actors/소희.jpg" alt="">
+      <div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><label><b>이름</b></label> 배사람 </li>
+          <li class="list-group-item"><label><b>닉네임</b></label>춤추는 산양 </li>
+          <li class="list-group-item"><label><b>이메일</b></label> bausa@gmail.com </li>
+          <li class="list-group-item"><label><b>전화번호</b></label> 010-5454-5454 </li>
+        </ul>
+      </div>
     </div>
   </template>
   
   <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
+.boardpage{
+  display: flex;
+}
   </style>
--- FTActors-fe/src/views/MontageDetail.vue
+++ FTActors-fe/src/views/MontageDetail.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="about">
-      <h1>상세 몽타쥬 페이지</h1>
-    </div>
-  </template>
+  <h1>상세 몽타쥬 페이지</h1>
+  <div class="montagepage">
+    <img src="@/assets/Next.png" alt="" id="previous">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/lJXaNYTVjrQ?si=jZHMoe0Tu1yo4tPb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<img src="@/assets/Next.png" alt="">
+</div>  
+</template>
   
   <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
+.montagepage{
+  display: flex;
+  align-items: center;
+}
+.montagepage img {
+  width: 25px;
+  height: 25px;
+}
+#previous {
+  transform: rotate(180deg);
+}
   </style>
