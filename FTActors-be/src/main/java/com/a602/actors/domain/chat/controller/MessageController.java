1a97ca879ebf1ebaaafcd0b260c8b206ba63751c
송정훈 <sjhjack@naver.com>
2024-03-12 13:37:22 +0900

    feat: 단체 채팅 구현

--- FTActors-be/src/main/java/com/a602/actors/domain/chat/controller/MessageController.java
+++ FTActors-be/src/main/java/com/a602/actors/domain/chat/controller/MessageController.java
@@ -7,17 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.a602.actors.domain.chat.dto.ChatMessageDto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MessageController {
 	private final SimpMessagingTemplate messagingTemplate;
 
-	@MessageMapping("/message")		// uri : pub/message
+	@MessageMapping("/message")		// uri : /pub/message
 	public void message(ChatMessageDto chatMessageDto){
 		if(chatMessageDto.getType().equals(ChatMessageDto.MessageType.ENTER)){
 			chatMessageDto.setMessage(chatMessageDto.getSender() + "님이 입장하셨습니다.");
 		}
+
+		log.info("convert to ========== {}", "/sub/room/" + chatMessageDto.getRoomId());
+		log.info("message    ========== {}", chatMessageDto.getMessage());
 
 		messagingTemplate.convertAndSend("/sub/room/" + chatMessageDto.getRoomId(), chatMessageDto.getMessage());
 	}
--- FTActors-be/src/main/java/com/a602/actors/domain/chat/dto/ChatMessageDto.java
+++ FTActors-be/src/main/java/com/a602/actors/domain/chat/dto/ChatMessageDto.java
@@ -1,10 +1,12 @@
 package com.a602.actors.domain.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ChatMessageDto {
 	public enum MessageType {
 		ENTER, TALK
