f1877a0fadc3fd2e6447d86338773e439f276e9c
송정훈 <sjhjack@naver.com>
2024-03-15 17:00:27 +0900

    chore: WebSocketConfig 위치 수정

--- FTActors-be/src/main/java/com/a602/actors/global/common/config/WebSocketConfig.java
+++ FTActors-be/src/main/java/com/a602/actors/global/common/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.a602.actors.global.config;
+package com.a602.actors.global.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
