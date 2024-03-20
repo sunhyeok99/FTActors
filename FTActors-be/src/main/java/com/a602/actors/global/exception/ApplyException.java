014c8d05adb7527a93852356b4d3eb27ff2f9e3d
김선혁 <kkn1312@naver.com>
2024-03-20 15:06:21 +0900

    feat : apply 예외처리 추가

---
+++ FTActors-be/src/main/java/com/a602/actors/global/exception/ApplyException.java
@@ -0,0 +1,7 @@
+package com.a602.actors.global.exception;
+
+public class ApplyException extends BaseExceptionAbs {
+    public ApplyException(ExceptionCodeSet exceptionCode) {
+        super(exceptionCode);
+    }
+}
