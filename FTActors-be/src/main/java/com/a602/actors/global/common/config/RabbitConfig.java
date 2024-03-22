4ef6f1ed7d40df2d1ec9097423b3b286fd30713f
송정훈 <sjhjack@naver.com>
2024-03-22 17:21:39 +0900

    refactor: RabbitMQ id pw를 config 파일로 분리

--- FTActors-be/src/main/java/com/a602/actors/global/common/config/RabbitConfig.java
+++ FTActors-be/src/main/java/com/a602/actors/global/common/config/RabbitConfig.java
@@ -9,6 +9,7 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,13 +19,25 @@ import lombok.extern.slf4j.Slf4j;
 @EnableRabbit
 @Slf4j
 public class RabbitConfig {
-	private static final String CHAT_QUEUE_NAME = "chat.queue";
-	private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
-	private static final String ROUTING_KEY = "*.room.*";
+	// private static final String CHAT_QUEUE_NAME = "chat.queue";
+	// private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
+	// private static final String ROUTING_KEY = "*.room.*";
+
+	// @Value("${rabbit.queue-name}")
+	// private static String CHAT_QUEUE_NAME;
+	// @Value("${rabbit.exchange-name}")
+	// private static String CHAT_EXCHANGE_NAME;
+	// @Value("${rabbit.routing-key}")
+	// private static String ROUTING_KEY;
+	// @Value("${rabbit.user_name}")
+	// private static String USER_NAME;
+	// @Value("${rabbit.password}")
+	// private static String PASSWORD;
+
 
 	// Queue 등록
 	@Bean
-	public Queue queue(){
+	public Queue queue(@Value("${rabbit.queue-name}") String CHAT_QUEUE_NAME){
 		log.info("queue 등록 !!");
 		// return new Queue(CHAT_QUEUE_NAME, true);
 		return new Queue(CHAT_QUEUE_NAME);
@@ -32,26 +45,26 @@ public class RabbitConfig {
 
 	// Exchange 등록
 	@Bean
-	public TopicExchange exchange(){
+	public TopicExchange exchange(@Value("${rabbit.exchange-name}") String CHAT_EXCHANGE_NAME){
 		log.info("exchange 등록 !!");
 		return new TopicExchange(CHAT_EXCHANGE_NAME);
 	}
 
 	// Exchange와 Queue 바인딩
 	@Bean
-	// public Binding binding(Queue queue, TopicExchange exchange){
-	public Binding binding(){
+	public Binding binding(Queue queue, TopicExchange exchange, @Value("${rabbit.routing-key}") String ROUTING_KEY){
+	// public Binding binding(){
 		log.info("binding 등록 !!");
 		return BindingBuilder
-			.bind(queue())
-			.to(exchange())
+			.bind(queue)
+			.to(exchange)
 			.with(ROUTING_KEY);
 	}
 
 	// RabbitMQ와의 메시지 통신을 담당하는 클래스
 	@Bean
-	public RabbitTemplate rabbitTemplate(){
-		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
 		rabbitTemplate.setMessageConverter(jsonMessageConverter());
 		log.info("rabbitTemplate 등록 !!");
 		return rabbitTemplate;
@@ -59,15 +72,15 @@ public class RabbitConfig {
 
 	// RabbitMQ와의 연결을 관리하는 클래스
 	@Bean
-	public ConnectionFactory connectionFactory(){
+	public ConnectionFactory connectionFactory(@Value("${rabbit.user_name}") String USER_NAME, @Value("${rabbit.password}") String PASSWORD){
 		log.info("connectionFactory 등록 !!");
 		CachingConnectionFactory factory = new CachingConnectionFactory();
 		factory.setHost("localhost");
 		factory.setPort(5672);
 		// factory.setPort(58153);
 		factory.setVirtualHost("/");	// ?? 이게 뭘까
-		factory.setUsername("guest");
-		factory.setPassword("guest");
+		factory.setUsername(USER_NAME);
+		factory.setPassword(PASSWORD);
 
 		log.info("host : {}", factory.getHost());
 		log.info("port : {}", factory.getPort());
