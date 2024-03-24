a6d519323839462ab4b7ff790702bd298c5016ab
송정훈 <sjhjack@naver.com>
2024-03-24 22:00:15 +0900

    refactor: uri 수정

--- FTActors-be/src/main/java/com/a602/actors/domain/chat/controller/ChatController.java
+++ FTActors-be/src/main/java/com/a602/actors/domain/chat/controller/ChatController.java
@@ -28,7 +28,7 @@ public class ChatController {
 		return new ApiResponse<>(HttpStatus.OK.value(), "채팅방 생성 완료", "");
 	}
 
-	@GetMapping("/all")
+	@GetMapping("/room/all")
 	public ApiResponse<List<ChatRoomDto>> getAllChatRooms(){
 		return new ApiResponse<>(HttpStatus.OK.value(), "모든 채팅방 리스트 조회 성공", chatService.findAllChatRooms());
 	}
@@ -39,7 +39,7 @@ public class ChatController {
 		return new ApiResponse<>(HttpStatus.OK.value(), "현재 참여중인 채팅방 리스트", chatService.getMyChatRoomList(memberId));
 	}
 
-	@GetMapping("/id")
+	@GetMapping("/room/id")
 	public ApiResponse<ChatRoomDto> getChatRoomByRoomId(@RequestParam Long roomId){
 		return new ApiResponse<>(HttpStatus.OK.value(), "id에 해당하는 채팅방 조회 성공", chatService.findRoomByRoomId(roomId));
 	}
