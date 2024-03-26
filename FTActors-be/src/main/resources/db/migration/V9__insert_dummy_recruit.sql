120d518edb204f012f74aac57dd1c8a6d5c305cd
김선혁 <kkn1312@naver.com>
2024-03-26 14:25:00 +0900

    fix : 파일 관련 변경

---
+++ FTActors-be/src/main/resources/db/migration/V9__insert_dummy_recruit.sql
@@ -0,0 +1,21 @@
+-- 1번 INSERT 문
+INSERT INTO recruitment (title, content, member_id, category, image, start_date, end_date, file)
+VALUES ('Title 1', 'Content 1', 1, 'Category 1', 'image1.jpg', '2024-03-25', '2024-04-25', 'file1.pdf');
+
+-- 2번 INSERT 문
+INSERT INTO recruitment (title, content, member_id, category, start_date, end_date)
+VALUES ('Title 2', 'Content 2', 2, 'Category 2', '2024-03-26', '2024-04-26');
+
+-- 3번 INSERT 문
+INSERT INTO recruitment (title, content, member_id, category, start_date, end_date)
+VALUES ('Title 3', 'Content 3', 3, 'Category 3', '2024-03-27', '2024-04-27');
+
+-- 4번 INSERT 문
+INSERT INTO recruitment (title, content, member_id, category, start_date, end_date)
+VALUES ('Title 4', 'Content 4', 4, 'Category 4', '2024-03-28', '2024-04-28');
+
+-- 5번 INSERT 문
+INSERT INTO recruitment (title, content, member_id, category, image, start_date, end_date, `file`)
+VALUES ('Title 5', 'Content 5', 5, 'Category 5', 'image5.jpg', '2024-03-29', '2024-04-29', 'file5.pdf');
+
+--------------------
