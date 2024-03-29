6c16878a7dcddb968e2f350fb4a8fbe3be154055
BaeJinHyeon <compuet33@gmail.com>
2024-03-29 13:46:15 +0900

    refactor: endpoint /api 제거

--- FTActors-be/src/main/java/com/a602/actors/domain/recruitment/controller/RecruitmentController.java
+++ FTActors-be/src/main/java/com/a602/actors/domain/recruitment/controller/RecruitmentController.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/recruitment")
+@RequestMapping("/recruitment")
 @RestController
 public class RecruitmentController {
 
