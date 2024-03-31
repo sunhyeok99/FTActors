9aff1e412919e1ea1f16b74e4371b02c4e7058a5
김도희 <catisaw99@gmail.com>
2024-04-01 03:10:03 +0900

    feat : 신고 거절 로직 추가 (미완)

--- FTActors-fe/src/views/adminview/BlacklistView.vue
+++ FTActors-fe/src/views/adminview/BlacklistView.vue
@@ -3,6 +3,11 @@
     <h2> 블랙리스트 총 {{blacklist.length}}명</h2>
     <div class="row row-cols-1 row-cols-md-2 g-4">
       <div class="col-lg-3" v-for="(black, index) in blacklist" :key="index" id="one">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
+          role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+          <rect width="100%" height="100%" fill="#777"></rect>
+          <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+        </svg>
         <h3>{{ black.name }}</h3> 
         <p>{{ black.email }}</p> 
         <p><a class="btn btn-secondary" :href="`#details${index}`">블랙리스트 해제</a></p> 
