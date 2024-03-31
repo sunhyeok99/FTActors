dbef6aed2f229a0e79a8a4753bae671e6a6c7f32
김도희 <catisaw99@gmail.com>
2024-04-01 04:17:29 +0900

    feat : 신고 상세 사유 추가

--- FTActors-fe/src/views/adminview/ReportView.vue
+++ FTActors-fe/src/views/adminview/ReportView.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="wrapper">
-    <h2>신고 총 {{reports.length}}개</h2>
+    <h2>접수된 신고 총 {{reports.length}}개</h2>
     <div class="row row-cols-1 row-cols-md-2 g-4">
-      <div class="col-lg-3" v-for="(report, index) in reports" :key="index" id="one">
-        <img :src="report.link" alt="" id="reportImage">
+      <!-- 신고 카드 -->
+      <div class="col-lg-3" v-for="(report, index) in reports" :key="index" id="one">        
+        <img :src="report.link" alt="" id="reportImage"  :data-bs-toggle="'modal'" :data-bs-target="`#reportDetailModal${index}`">
         <h3>{{ report.reporteeId }}</h3> 
         <p>{{ report.reporterId }} - {{ report.reason }}</p> 
+        <!-- 신고 상세 모달 -->
+        <div class="modal fade" :id="`reportDetailModal${index}`" tabindex="-1" aria-labelledby="reportDetailModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="reportDetailModalLabel">{{report.reporterId}} 님의 {{report.reporteeId}} 신고 사유</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <img :src="report.link" alt="" id="reportImageDetail">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+
+              </div>
+            </div>
+          </div>
+        </div>
         <div class="accept-rejection">
-        <p><a class="btn btn-secondary" id="accept" :href="`#details${index}`" @click="acceptReport(report.reporteeId)">신고 수락</a></p> 
-        <p><a class="btn btn-secondary" id="rejection" :href="`#details${index}`">신고 거절</a></p> 
+        <p><a class="btn btn-secondary" id="accept" :href="`#details${index}`" @click="acceptReport(report.reporteeId)" >신고 수락</a></p> 
+        <p><a class="btn btn-secondary" id="rejection" :href="`#details${index}`" @click="rejectReport(report.reporteeId)" >신고 거절</a></p> 
       </div>
       </div>
     </div>
+    
   </div>
 </template>
 
@@ -49,6 +69,22 @@ const acceptReport = (reporteeId) => {
     });
 };
 
+const rejectReport = (reporteeId) => {
+  const body = {
+    reportId : reporteeId
+  }
+  console.log(reporteeId)
+  axios.post(`http://localhost:8080/admin/report/rejection`, body)
+    .then((response) => {
+      alert(response)
+      console.log('###############',reporteeId)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+
 
 
 </script>
@@ -75,5 +111,9 @@ margin: 5px;
 
 #reportImage{
   width: 250px;
+}
+#reportImageDetail{
+  width: 100%;
+  height: 100%;
 }
 </style>
