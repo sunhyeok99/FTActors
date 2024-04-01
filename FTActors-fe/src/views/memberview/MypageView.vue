6ab84ce685690d58e192aa7810faae278a483c69
김선혁 <kkn1312@naver.com>
2024-04-02 03:39:26 +0900

    feat : 마이페이지 ( 몽타주 , 프로필뺴고)

--- FTActors-fe/src/views/memberview/MypageView.vue
+++ FTActors-fe/src/views/memberview/MypageView.vue
@@ -4,7 +4,7 @@
       <h1><b>마이페이지</b></h1>
     </div>
     <div class="profile">
-      <div class="profilephoto"> <img src="@/assets/icons/DefaultProfile.png" alt=""></div>
+      <div class="profilephoto">  <img :src="member.image" alt=""></div>
       <div class="profilelist">
         <ul class="list-group list-group-flush">
           <li class="list-group-item"><label><b>이름</b></label> 배사람
@@ -32,15 +32,9 @@
             <p class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false"
               id="highest">공고</p>
             <ul class="dropdown-menu">
-              <li>
-                <p class="dropdown-item" @click="getWishlist">찜한 공고</p>
-              </li>
-              <li>
-                <p class="dropdown-item" @click="getApplylist">지원한 공고</p>
-              </li>
-              <li>
-                <p class="dropdown-item" @click="getPostlist">게시한 공고</p>
-              </li>
+              <li><p class="dropdown-item" @click="getWishlist">찜한 공고</p></li>
+              <li><p class="dropdown-item" @click="getApplylist">지원한 공고</p></li>
+              <li><p class="dropdown-item" @click="getPostlist">게시한 공고</p></li>
             </ul>
           </button>
           <!-- 몽타쥬 -->
@@ -53,10 +47,10 @@
               id="highest">동료</p>
             <ul class="dropdown-menu">
               <li>
-                <p class="dropdown-item">팔로우</p>
+                <p class="dropdown-item" @click="getFollowingList">팔로우</p>
               </li>
               <li>
-                <p class="dropdown-item">팔로잉</p>
+                <p class="dropdown-item" @click="getFollowerList">팔로잉</p>
               </li>
 
             </ul>
@@ -64,57 +58,81 @@
         </div>
       </nav>
       <!-- 마이페이지 탭 콘텐츠 -->
-      <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"
-          tabindex="0">
-          <ProfileTab />
-          </div>
-        <div class="tab-pane fade" id="nav-board" role="tabpanel" aria-labelledby="nav-board-tab" tabindex="1" v-show="recruitments.length > 0">
-          <div class="col" v-for="recruitment in recruitments" :key="recruitment.id">
-          <div class="card" id="board">
-          <img :src= "recruitment.image" alt="" @click="goToBoardDetail(recruitment.id)">
-           <div class="card-body" @click="goToBoardDetail(recruitment.id)">
-          <h5 class="card-title"><b>{{ recruitment.title }}</b></h5>
-          <p class="card-text">종료일자: {{ recruitment.endDate }}</p>
-         </div>
-          </div>
-         </div>
-      </div>
-
-      <div class="tab-pane fade" id="nav-applylist" role="tabpanel" aria-labelledby="nav-board-tab" tabindex="1" v-show="applys.length > 0">
-          <div class="col" v-for="apply in applys" :key="apply.id">
-          <div class="card" id="apply">
-           <div class="card-body" @click="goToApplyDetail(apply.id)">
-          <h5 class="card-title"><b>공고 제목 : {{ apply.recruitmentTitle }}</b></h5>
-          <p class="card-text">지원날짜: {{ apply.createdAt }}</p>
-          <h5><b>첨부파일</b></h5>
-            <p>
-               <a :href="apply.videoLink" download="apply_file">파일 다운로드</a>
-              </p>
-         </div>
-          </div>
-         </div>
-      </div>
-
-      <div class="tab-pane fade" id="nav-postlist" role="tabpanel" aria-labelledby="nav-board-tab" tabindex="1" v-show="posts.length > 0">
-          <div class="col" v-for="post in posts" :key="post.id">
-          <div class="card" id="posts">
-          <img :src= "post.image" alt="" @click="goToBoardDetail(post.id)">
-           <div class="card-body" @click="goToBoardDetail(post.id)">
-          <h5 class="card-title"><b>{{ post.title }}</b></h5>
-          <p class="card-text">종료일자: {{ post.endDate }}</p>
-         </div>
-          </div>
-         </div>
-      </div>
-
-        <div class="tab-pane fade" id="nav-montage" role="tabpanel" aria-labelledby="nav-montage-tab" tabindex="2">
-          <MontageTab />
+<div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"
+        tabindex="0">
+        <ProfileTab />
+    </div>
+    <div class="tab-pane fade show active" id="nav-board" role="tabpanel" aria-labelledby="nav-board-tab" tabindex="1"
+        v-show="recruitments.length > 0">
+        <div class="col" v-for="recruitment in recruitments" :key="recruitment.id">
+            <div class="card" id="board">
+                <img :src="recruitment.image" alt="" @click="goToBoardDetail(recruitment.id)">
+                <div class="card-body" @click="goToBoardDetail(recruitment.id)">
+                    <h5 class="card-title"><b>{{ recruitment.title }}</b></h5>
+                    <p class="card-text">종료일자: {{ recruitment.endDate }}</p>
+                </div>
+            </div>
         </div>
-        <div class="tab-pane fade" id="nav-companion" role="tabpanel" aria-labelledby="nav-companion-tab" tabindex="3">
-          <CompanionTab />
+    </div>
+    <div class="tab-pane fade show active" id="nav-applylist" role="tabpanel" aria-labelledby="nav-apply-tab" tabindex="1"
+        v-show="applys.length > 0">
+        <div class="col" v-for="apply in applys" :key="apply.id">
+            <div class="card" id="apply">
+                <div class="card-body" @click="goToApplyDetail(apply.id)">
+                    <h5 class="card-title"><b>공고 제목 : {{ apply.recruitmentTitle }}</b></h5>
+                    <p class="card-text">지원날짜: {{ apply.createdAt }}</p>
+                    <h5><b>첨부파일</b></h5>
+                    <p>
+                        <a :href="apply.videoLink" download="apply_file">파일 다운로드</a>
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
+    </div>
+    <div class="tab-pane fade show active" id="nav-postlist" role="tabpanel" aria-labelledby="nav-post-tab" tabindex="1"
+        v-show="posts.length > 0">
+        <div class="col" v-for="post in posts" :key="post.id">
+            <div class="card" id="posts">
+                <img :src="post.image" alt="" @click="goToBoardDetail(post.id)">
+                <div class="card-body" @click="goToBoardDetail(post.id)">
+                    <h5 class="card-title"><b>{{ post.title }}</b></h5>
+                    <p class="card-text">종료일자: {{ post.endDate }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade" id="nav-montage" role="tabpanel" aria-labelledby="nav-montage-tab" tabindex="2">
+        <MontageTab />
+    </div>
+    <div class="tab-pane fade" id="nav-companion" role="tabpanel" aria-labelledby="nav-companion-tab"
+        tabindex="3">
+        <div class="tab-pane fade show active" id="nav-postlist" role="tabpanel" aria-labelledby="nav-follow-tab" tabindex="1"
+            v-if="followings.length > 0">
+            <div class="col" v-for="following in followings" :key="following.id">
+                <div class="card-body" @click="goToProfileDetail(following.id)">
+                    <h5 class="card-title"><b>{{ following.memberName }}</b></h5>
+                    <button class="like-btn" @click="changeFollow(following.followingId, following.followerId)">
+                        <p v-if="following.follow === 1">삭제</p>
+                        <p v-else>팔로잉</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade show active" id="nav-postlist" role="tabpanel" aria-labelledby="nav-follow-tab" tabindex="1"
+            v-if="followers.length > 0">
+            <div class="col" v-for="follower in followers" :key="follower.id">
+                <div class="card-body" @click="goToProfileDetail(follower.id)">
+                    <h5 class="card-title"><b>{{ follower.memberName }}</b></h5>
+                    <button class="like-btn" @click="changeFollow(follower.followingId, follower.followerId)">
+                        <p v-if="follower.follow === 1">삭제</p>
+                        <p v-else>팔로잉</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     </div>
   </div>
 </template>
@@ -138,6 +156,9 @@ const member = ref({});
 const recruitments = ref({});
 const applys = ref({});
 const posts = ref({});
+
+const followings = ref({});
+const followers = ref({});
 
 
 const isPrivate = ref(false)
@@ -202,6 +223,28 @@ const getPostlist = async () => {
   }
 };
 
+const getFollowingList = async () => {
+  try {
+    const response = await followApi.followingList(loginMember.value);
+    console.log(response.data);
+    followings.value = response.data.data;
+    followers.value = [];
+  } catch (error) {
+    console.error("Error fetching wishlist:", error);
+  }
+};
+
+const getFollowerList = async () => {
+  try {
+    const response = await followApi.followerList(loginMember.value);
+    console.log(response.data);
+    followers.value = response.data.data;
+    followings.value = [];
+  } catch (error) {
+    console.error("Error fetching wishlist:", error);
+  }
+};
+
 const goToBoardDetail = (boardId) => {
   router.push({ name: 'boardDetail', params: { id: boardId } });
 };
@@ -210,7 +253,29 @@ const goToApplyDetail = (applyId) => {
   router.push({ name: 'applyDetail', params: { id: applyId } });
 };
 
-
+const changeFollow = async (followingId, followerId, index) => {
+  try {
+      const response = await followApi.following(followingId, followerId);
+      if(response.data.status == 200){
+        if(response.data.data == 0){
+          alert('팔로잉을 취소하였습니다.')
+        }
+        else{
+          alert('팔로잉을 하였습니다.')
+        }
+        const tmp = followings.value.find(following => following.id === index);
+        if(tmp == 0 || tmp == null){
+          tmp.follow = response.data.data;   
+          // 여기 다시 해야함
+        }
+        tmp.follow = response.data.data;
+        console.log(tmp.follow)
+      }
+    
+    } catch (error) {
+      console.error('Error toggling like:', error);
+  }
+};
 
 </script>
 
