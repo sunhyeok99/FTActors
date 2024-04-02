c34b483e41a0af87ce6e2cf2bfcbcb887afafb0c
김도희 <catisaw99@gmail.com>
2024-04-03 02:12:32 +0900

    design : 소소 디자인

--- FTActors-fe/src/views/memberview/JoinView.vue
+++ FTActors-fe/src/views/memberview/JoinView.vue
@@ -70,7 +70,10 @@
                 </label>
               </div>
             </div>
-            <button :disabled="!isFormValid" class="btn btn-primary w-100 mb-2" @click.prevent="signup">회원가입</button>
+            <button :disabled="!isFormValid" class="btn btn-dark w-100 mb-2"ㄴ type="button" disabled @click.prevent="signup">
+              <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+              <span role="status">회원가입</span>
+            </button>
             <button type="button" class="btn w-100" @click="goToLogin">이미 회원이라면?</button>
           </form>
         </div>
--- FTActors-fe/src/views/memberview/LoginView.vue
+++ FTActors-fe/src/views/memberview/LoginView.vue
@@ -15,7 +15,7 @@
               <label for="password">비밀번호</label>
               <input :type="showPassword ? 'text' : 'password'" id="password" v-model="password" class="form-control" placeholder="비밀번호를 입력해주세요">
             </div>
-            <button type="button" class="btn btn-primary w-100 mb-2" @click="login">로그인</button>
+            <button type="button" class="btn btn-dark w-100 mb-2" @click="login">로그인</button>
             <button type="button" class="btn w-100" @click="goToJoin">아직 회원이 아니라면? </button>
           </form>
         </div>
