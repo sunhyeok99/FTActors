def2cbaffe8435005c043df8a26dbe21416d08d2
김도희 <catisaw99@gmail.com>
2024-04-02 13:05:37 +0900

    feat : 나머지 전부 푸시

--- FTActors-fe/src/components/common/CommentReply.vue
+++ FTActors-fe/src/components/common/CommentReply.vue
@@ -6,15 +6,16 @@
           <div class="columnthings">
             <div class="rowthings">
               <!-- 좋아요 버튼 -->
-              <button class="like-btn" :class="{ liked: isLiked }" @click.stop="toggleLike()">
+              <button class="like-btn" :class="{ liked: isLiked }" @click.stop="toggleLike() ,addLike()">
                 <img v-if="isLiked" src="@/assets/icons/like-filled.png" alt="Liked">
                 <img v-else src="@/assets/icons/like-outline.png" alt="Unliked">
+                <p>{{ montageInfo.likeCount }}</p>
               </button>
               <!-- 신고 버튼 -->
-              <button type="button" class="report-btn" data-bs-toggle="modal" data-bs-target="#reportModal">
+              <button type="button" class="report-btn" data-bs-toggle="modal" data-bs-target="#reportModal" >
                 <img src="@/assets/icons/Scissors.png" alt="cut">
               </button>
-              <ReportModal />
+              <ReportModal :current-id="currentId" />
               <!-- 댓글 펼치기 -->
               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                 data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne"
@@ -42,19 +43,23 @@
                   <div class="d-flex w-100 align-items-center justify-content-between reply-block">
                     <strong class="mb-1" id="reply-member">{{ comment.memberId }}</strong>
                     <div class="col-10 mb-1 small comment-text">{{ comment.content }}</div>
+                    <!-- 삭제 버튼 -->
                     <div class="remove"><img src="@/assets/icons/Remove.png" alt=""
-                        @click="deleteComment(comment.commentId)"></div>
+                        @click.stop="deleteComment(comment.commentId)"></div>
                   </div>
                   <!-- 대댓글 작성 인풋 -->
-                  <div v-if="selectedComment === comment" class="mt-4 reply-container" @click.stop>
+                  <div v-if="selectedComment === comment" class="mt-4 reply-container">
                     <input type="text" class="form-control reply-input" placeholder="대댓글 작성"
-                      aria-label="Recipient's username" aria-describedby="reply-button" v-model="addReply">
+                      aria-label="Recipient's username" aria-describedby="reply-button" v-model="addReply" @click.stop>
                     <button class="btn btn-outline-secondary reply-btn" type="button" id="reply-button"
-                      @click="uploadReply()">작성</button>
+                      @click.stop="uploadReply()">작성</button>
                   </div>
                   <!-- 대댓글 리스트 -->
                   <div v-for="(reply, rIndex) in comment.reply" :key="`reply-${rIndex}`" class="mt-2" id="reply">
                     <p>ㄴ <b id="reply-member">{{ reply.memberId }}</b> {{ reply.content }}</p>
+                    <!-- 삭제 버튼 -->
+                    <div class="remove"><img src="@/assets/icons/Remove.png" alt=""
+                      @click.stop="deleteReply(reply.commentId)"></div>
                   </div>
                 </button>
               </div>
@@ -70,7 +75,7 @@
 
 <script setup>
 
-import { onMounted, ref, defineProps, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import axios from 'axios';
 import ReportModal from '@/components/modals/ReportModal.vue';
 
@@ -82,10 +87,36 @@ const props = defineProps({
 const isLiked = ref(false);
 const toggleLike = () => {
   isLiked.value = !isLiked.value;
+  if (isLiked.value) {
+    montageInfo.value.likeCount += 1;
+  } else {
+    if (montageInfo.value.likeCount > 0) {
+      montageInfo.value.likeCount -= 1;
+    }
+  }
 };
 
 // 댓글 저장
 const comments = ref([]);
+const montageInfo = ref([]);
+
+// 몽타쥬정보 가져오기
+const getMontageInfo = () => {
+  axios.get(`http://localhost:8080/montage/list`)
+    .then((response) => {
+      const found = response.data.data.find(m => m.id === props.currentId); // 여기서 m.id가 몽타쥬의 ID와 일치하는지 확인
+      if (found) {
+        montageInfo.value = found;
+        console.log('몽타쥬아이디:', props.currentId);
+        console.log('좋아요개수:', montageInfo.value.likeCount);
+      } else {
+        alert('해당 컨텐츠를 찾을 수 없습니다.');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 // 댓글/대댓글 가져오는 메서드
 const getCommentReply = () => {
@@ -99,21 +130,31 @@ const getCommentReply = () => {
     });
 };
 
-// 일단 페이지 로드 시 실행
-onMounted(() => {
-  getCommentReply();
-});
-
-// currentId 변화 감시
-watch(() => props.currentId, (newId, oldId) => {
-  // currentId가 바뀌면 getCommentReply 함수 호출
-  getCommentReply();
-}, { immediate: true }); // immediate: true 옵션을 사용하면 컴포넌트가 마운트될 때도 감시 함수가 즉시 실행됩니다.
-
 // 페이지 로드 시 getCommentReply 함수 실행
 onMounted(() => {
   getCommentReply();
+  getMontageInfo();
 });
+
+// 좋아요 추가하기
+const addLike = () => {
+    axios
+      .post(`http://localhost:8080/montage/${props.currentId}/like`)
+      .then((response) => {
+        console.log(response.data.data);
+        isLiked.value = response.data.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+};
+
+// currentId 변화 감지시 getCommentReply 함수 실행
+watch(() => props.currentId, (newId, oldId) => {
+  getCommentReply();
+  getMontageInfo();
+  addLike();
+}, { immediate: true }); 
 
 // 댓글 추가하기
 const addComment = ref("");
@@ -144,17 +185,6 @@ const uploadComment = () => {
 };
 
 const deleteComment = (id) => {
-  axios
-    .delete(`http://localhost:8080/montage/${props.currentId}/comment/${id}`)
-    .then(() => {
-      console.log("댓글이 삭제되었습니다:");
-      getCommentReply();
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-const deleteReply = (id) => {
   axios
     .delete(`http://localhost:8080/montage/${props.currentId}/comment/${id}`)
     .then(() => {
@@ -206,7 +236,18 @@ const uploadReply = () => {
   }
 };
 
-
+// 대댓글 삭제
+const deleteReply = (id) => {
+  axios
+    .delete(`http://localhost:8080/montage/${props.currentId}/comment/${id}`)
+    .then(() => {
+      console.log("대댓글이 삭제되었습니다:");
+      getCommentReply();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 </script>
 
@@ -231,9 +272,7 @@ const uploadReply = () => {
   flex-direction: row;
 }
 
-.accordion-button {
-  width: 25%;
-}
+
 
 .columnthings {
   display: flex;
@@ -243,6 +282,7 @@ const uploadReply = () => {
 .rowthings {
   display: flex;
   flex-direction: row;
+  height: auto;
 }
 
 
@@ -252,8 +292,8 @@ const uploadReply = () => {
 }
 
 .like-btn img {
-  width: 30px;
-  height: 30px;
+  width: 20px;
+  height: 20px;
 }
 
 .report-btn {
@@ -280,8 +320,14 @@ p {
 
 #reply {
   margin-left: 20px;
-}
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
+}
+#reply img{
+  margin-left: 20px;
+}
 #addreply {
   margin-left: 20px;
   width: auto;
--- FTActors-fe/src/components/modals/ReportModal.vue
+++ FTActors-fe/src/components/modals/ReportModal.vue
@@ -1,46 +1,87 @@
-
-
 <template>
-    <div class="modal fade" id="reportModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
-      <div class="modal-dialog" @click.stop>
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">신고</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  <div class="modal fade" id="reportModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+    data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog" @click.stop>
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">신고</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          신고사유
+          <div class="form-floating">
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example" ref="reasonSelect">
+              <option value="0" selected>구분</option>
+              <option value="1">타인을 비방하거나, 모욕함</option>
+              <option value="2">감독의 권한을 남용함</option>
+              <option value="3">사용 목적에 맞지 않는 사용자</option>
+            </select>   
+            <label for="floatingSelect">선택</label>
           </div>
-          <div class="modal-body">
-            신고사유
-            <div class="form-floating">
-                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-                  <option selected>구분</option>
-                  <option value="1">타인을 비방하거나, 모욕함</option>
-                  <option value="2">감독의 권한을 남용함</option>
-                  <option value="3">사용 목적에 맞지 않는 사용자</option>
-                </select>
-                <label for="floatingSelect">선택</label>
-              </div>
-              <div class="form-floating">
-                <textarea class="form-control" placeholder="신고 내용" id="floatingTextarea2" style="height: 100px"></textarea>
-                <label for="floatingTextarea2">세부 내용</label>
-              </div>
-              <div class="input-group">
-                <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
-                <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">업로드</button>
-              </div>
+          <div class="form-floating">
+            <textarea class="form-control" placeholder="신고 내용" id="floatingTextarea2" style="height: 100px"  ref="detailText"></textarea>
+            <label for="floatingTextarea2">세부 내용</label>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">신고 보내기</button>
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소</button>
-           
+          <div class="input-group">
+            <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04"
+              aria-label="Upload">
+            <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">업로드</button>
           </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="uploadReport">신고 보내기</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소</button>
+
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style>
-  
-  </style>
+  </div>
+</template>
+
+<script setup>
+import {  onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const reasonSelect = ref(null);
+const detailText = ref(null)
+const props = defineProps({
+  currentId: Number
+});
+console.log('커런트아이디',props.currentId)
+const uploadReport = () => {
+  const formData = new FormData();
+  // 신고 사유 추가
+  if (reasonSelect.value && reasonSelect.value.value) {
+    formData.append('reason', reasonSelect.value.value);
+  }
+  // 세부 내용 추가
+  if (detailText.value) {
+    formData.append('details', detailText.value.value);
+  }
+  // 파일 추가
+  const fileInput = document.querySelector('input[type="file"]');
+  if (fileInput && fileInput.files[0]) {
+    formData.append('file', fileInput.files[0]);
+  }
+
+  axios.post(`http://localhost:8080/montage/${props.currentId}/report`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+};
+
+
+onMounted(() => {
+  props.currentId;
+});
+
+</script>
+
+<style></style>
