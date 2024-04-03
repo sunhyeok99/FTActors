a5298ba6cb4c448d4dcf925f9644718e2daf6941
BaeJinHyeon <compuet33@gmail.com>
2024-04-03 17:58:13 +0900

    feat: dummy data 추가 및

---
+++ FTActors-be/src/main/resources/db/migration/V14__add_member_dummy_data.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO member(email, birth, gender, name, stage_name) VALUES('bjw@ssafy.com', '1999-01-11', 'M', '배정우', '농구맨');
+INSERT INTO member(email, birth, gender, name, stage_name) VALUES('bjh@ssafy.com', '1998-08-24', 'F', '배진현', '배피티');
+INSERT INTO member(email, birth, gender, name, stage_name) VALUES('sjh@ssafy.com', '1999-01-15', 'M', '송정훈', '송신');
+INSERT INTO member(email, birth, gender, name, stage_name) VALUES('psy@ssafy.com', '1995-11-11', 'F', '박소영', '싸피다니엘');
+INSERT INTO member(email, birth, gender, name, stage_name) VALUES('ksh@ssafy.com', '1999-01-31', 'M', '김선혁', '볼링맨');
+INSERT INTO member(email, birth, gender, name, stage_name) VALUES('kdh@ssafy.com', '1996-07-20', 'F', '김도희', '춤신춤왕');
+
