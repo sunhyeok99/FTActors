fb16f9e460f44aba3975577ef047be5f74b58d74
김선혁 <kkn1312@naver.com>
2024-04-04 11:59:13 +0900

    Deleted FTActors-be/gradle/wrapper/gradle-wrapper.jar, FTActors-be/gradle/wrapper/gradle-wrapper.properties, FTActors-be/src/main/java/com/a602/actors/domain/member/controller/MemberController.java, FTActors-be/src/main/java/com/a602/actors/domain/member/repository/MemberRepository.java, FTActors-be/src/main/java/com/a602/actors/domain/member/service/MemberService.java, FTActors-be/src/main/java/com/a602/actors/domain/member/service/MemberServiceImpl.java, FTActors-be/src/main/java/com/a602/actors/domain/member/Member.java, FTActors-be/src/main/java/com/a602/actors/domain/montage/controller/MontageController.java, FTActors-be/src/main/java/com/a602/actors/domain/montage/dto/MontageDto.java, FTActors-be/src/main/java/com/a602/actors/domain/montage/entity/Montage.java, FTActors-be/src/main/java/com/a602/actors/domain/montage/repository/MontageResourceRepository.java, FTActors-be/src/main/java/com/a602/actors/domain/montage/service/MontageFileService.java, FTActors-be/src/main/java/com/a602/actors/domain/profile/controller/ProfileController.java, FTActors-be/src/main/java/com/a602/actors/domain/profile/dto/Profile.java, FTActors-be/src/main/java/com/a602/actors/domain/profile/repository/ProfileCustomRepository.java, FTActors-be/src/main/java/com/a602/actors/domain/profile/repository/ProfileCustomRepositoryImpl.java, FTActors-be/src/main/java/com/a602/actors/domain/profile/repository/ProfileRepository.java, FTActors-be/src/main/java/com/a602/actors/domain/profile/service/ProfileService.java, FTActors-be/src/main/java/com/a602/actors/domain/profile/service/ProfileServiceImpl.java, FTActors-be/src/main/java/com/a602/actors/domain/recruitment/entity/Recruitment.java, FTActors-be/src/main/java/com/a602/actors/global/common/config/S3Config.java, FTActors-be/src/main/java/com/a602/actors/global/common/dto/ApiResponse.java, FTActors-be/src/main/java/com/a602/actors/global/common/entity/BaseEntity.java, FTActors-be/src/main/java/com/a602/actors/FtActorsApplication.java, FTActors-be/src/main/resources/db/migration/V1__init.sql, FTActors-be/src/main/resources/application.yml, FTActors-be/src/test/java/com/a602/actors/FtActorsApplicationTests.java, FTActors-be/.gitignore, FTActors-be/build.gradle, FTActors-be/gradlew, FTActors-be/gradlew.bat, FTActors-be/settings.gradle

--- FTActors-be/src/main/java/com/a602/actors/domain/profile/repository/ProfileCustomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.a602.actors.domain.profile.repository;
-
-import com.a602.actors.domain.profile.dto.Profile;
-
-public interface ProfileCustomRepository {
-    Profile findById(Long memberId);
-}
